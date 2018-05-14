34e1676c95297243d0c2201d0ebc70166cd0cdc8
VITRE Anthony <anthony.vitre2016@campus-eni.fr>
2018-05-14 11:57:11 +0200

    Couche

---
+++ BO/Model/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Event4U.Models
+{
+    public class Event
+    {
+        public string nom { get; set; }
+        public string address{ get ;set; }
+        public DateTime date { get; set;}
+        public string descriptif { get; set; }
+    }
+}
